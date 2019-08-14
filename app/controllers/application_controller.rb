fa7e7dc39397ff4b70ae460cdaa7a2f93efa4106
Kazuho Shibuya <businessfive25@gmail.com>
2019-08-14 21:01:40 +0900

    Improve the controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "Hola, mundo!"
   end
 
-  def goodby
+  def goodbye
     render html: "goodbye, world!"
   end
 end
